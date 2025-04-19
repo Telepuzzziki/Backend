0b41cd5a9c88543f11ec0101ed77671e2eb83bf1
Alohamaan <131455740+Alohamaan@users.noreply.github.com>
2025-04-19 05:13:13 +0300

    added cors middleware to main.py file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.endpoints.csv_upload import router as csv_upload_router
 from app.endpoints.validate_dataset import router as validate_dataset_router
 
@@ -6,6 +7,14 @@ app = FastAPI(
     title="Data Processing API",
     description="Uploads CSV files and processes them using validation rules.",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")

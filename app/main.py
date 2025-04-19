e499a677567ce92599a4f2ae6188e68b2abdfde3
lendy <vasilkovarsenij3@gmail.com>
2025-04-19 11:46:55 +0000

    added_compose

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.endpoints.csv_upload import router as csv_upload_router
 from app.endpoints.validate_dataset import router as validate_dataset_router
 
+
 app = FastAPI(
     title="Data Processing API",
     description="Uploads CSV files and processes them using validation rules.",
@@ -26,4 +27,4 @@ app.include_router(validate_dataset_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
